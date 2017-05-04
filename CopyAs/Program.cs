9c07a82e3cc2ff2836a5c3b9a92da266e43aea51
fred <domaine75@gmail.com>
2017-05-04 11:14:04 +0200

    Update Program.cs

--- CopyAs/Program.cs
+++ CopyAs/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Security;
 
 namespace CopyAs
@@ -8,29 +11,81 @@ namespace CopyAs
   {
     private static void Main(string[] arguments)
     {
-      string[] userInfo = GetDataFromDb();
-      StartProcess(userInfo);
+      // arguments[0] = username to get data from DB
+      // arguments[1] = password  to get data from DB
+      // arguments[2] = domain to get data from DB; local domain by default (pc's name)
+      // arguments[3] = chemin source
+      // arguments[4] = chemin destination
+      // arguments[5] = fichiers sources à copier list<string>
+      
+      string[] userInfo = GetDataFromDb(); // read ini file to get credentials
+
+      List<string> lcrFiles = arguments.ToList();
+      if (CreateBatchFile("copyFiles.bat", lcrFiles, userInfo[3]))
+      {
+        StartProcess(userInfo, "copyFiles.bat", "");
+      }
+      
       Console.WriteLine("Press anykey to exit:");
       Console.ReadKey();
+    }
+
+    private static bool CreateBatchFile(string batchFileName, List<string> filesToBeCopied, string destinationPath )
+    {
+      bool result = false;
+      try
+      {
+        using (StreamWriter sw = new StreamWriter(batchFileName, false))
+        {
+          foreach (string file in filesToBeCopied)
+          {
+            sw.WriteLine($"copy {file} {AddSlash(destinationPath)}{file}");
+          }
+        }
+
+        result = true;
+      }
+      catch (Exception exception)
+      {
+        Console.WriteLine($"There was an error: {exception}");
+        result = false;
+      }
+
+      return result;
+    }
+
+    private static string AddSlash(string filePath)
+    {
+      return filePath.EndsWith("\\") ? filePath : filePath + "\\";
     }
 
     private static string[] GetDataFromDb()
     {
       string[] result = new string[4];
+      // get credentials from ini file
+      // result[0] = User Name
+      // result[1] = Password
+      // result[2] = Domain
+      // result[3] = Export Path
       // add code to get data from DB
+      //for now to test we input manually
+      result[0] = "username";
+      result[1] = "my super password";
+      result[2] = "domain_name";
+      result[3] = @"\\servername\drive$\directory";
       return result;
     }
 
-    private static void StartProcess(string[] userInfo)
+    private static void StartProcess(string[] userInfo, string processFileName, string processArgument)
     {
       Process proc = new Process();
       SecureString ssPwd = new SecureString();
       proc.StartInfo.UseShellExecute = false;
-      proc.StartInfo.FileName = userInfo[0]; //"filename";
-      proc.StartInfo.Arguments = userInfo[1]; // "args...";
+      proc.StartInfo.FileName = processFileName; //"filename";
+      proc.StartInfo.Arguments = processArgument; // "args...";
       proc.StartInfo.Domain = userInfo[2]; // "domainname";
-      proc.StartInfo.UserName = userInfo[3]; //"username";
-      string password = "user entered password";
+      proc.StartInfo.UserName = userInfo[0]; //"username";
+      string password = userInfo[1]; // password
       foreach (char c in password)
       {
         ssPwd.AppendChar(c);
